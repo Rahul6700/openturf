2a8179e78627a2071f8fe3c0501d6981aca03ed4
Rahul6700 <rahulsk4087@gmail.com>
2025-06-11 13:28:14 +0530

    feat: setup mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+//connecting mongodb
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('connected to mongoDB'))
+.catch(err => {
+  console.error('error connecting to mongoDB:', err);
+  process.exit(1);
+});
 
 app.get('/', (req, res) => {
   res.send('hello world');
